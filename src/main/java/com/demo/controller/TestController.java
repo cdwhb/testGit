29b78f204142c33b2ea833623f57a252a0b3d536
MaDeNong <1596128342@qq.com>
2018-10-28 21:01:10 +0800

    注释所有注解

--- src/main/java/com/demo/controller/TestController.java
+++ src/main/java/com/demo/controller/TestController.java
@@ -1,13 +1,18 @@
 package com.demo.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+//@RestController
 public class TestController {
 
-    @RequestMapping(value = "/hi")
+    //@RequestMapping(value = "/hi")
     public String hi() {
         return "hello world!";
+    }
+
+    //@RequestMapping(value = "/hii")
+    public String hii() {
+        return "hello world 02!";
     }
 }

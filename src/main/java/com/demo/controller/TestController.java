fa864a1117f83fd8cbec5e77815388bfb344ce7b
codewhb <1976671726@qq.com>
2018-10-28 17:04:55 +0800

    新增TestController

---
+++ src/main/java/com/demo/controller/TestController.java
@@ -0,0 +1,13 @@
+package com.demo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @RequestMapping(value = "/hi")
+    public String hi() {
+        return "hello world!";
+    }
+}

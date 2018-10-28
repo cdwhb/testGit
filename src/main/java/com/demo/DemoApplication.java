be5ec21e6ffd9318c00fdbf48ffa0d027b0a1cab
MaDeNong <1596128342@qq.com>
2018-10-28 22:38:29 +0800

    注释注解

--- src/main/java/com/demo/DemoApplication.java
+++ src/main/java/com/demo/DemoApplication.java
@@ -1,12 +1,12 @@
 package com.demo;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+//@SpringBootApplication
 public class DemoApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
+       // SpringApplication.run(DemoApplication.class, args);
     }
 }
